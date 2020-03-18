1dd944616251ec5365243f37dcbc85d44fe4194c
UmarGit <umarsheikh303@gmail.com>
2020-03-19 03:24:58 +0500

    [message]

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 from flask import Flask,render_template
 from a import api_gt
 from b import update
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
