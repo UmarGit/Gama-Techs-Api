7251b08379d868901a78d2cc94c7c762a751c0e5
UmarGit <umarsheikh303@gmail.com>
2020-03-22 22:32:16 +0500

    update

--- b.py
+++ b.py
@@ -6,5 +6,6 @@ def update():
   "send_email": "1"
   }
   r = requests.post("https://www.parsehub.com/api/v2/projects/tb8TVjOOkGtq/run", data=params2)
+  r2 = requests.post("https://www.parsehub.com/api/v2/projects/tMsTu14kY6Oe/run", data=params2)
   content = '<h1>=>>>> DATA UPDATED SUCCESSFULLY</h1>'
   return content
